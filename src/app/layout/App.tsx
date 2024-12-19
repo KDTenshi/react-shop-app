4deaac3ebe574e6c0f3f59fe9ec11f9858538836
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-19 17:47:55 +0300

    Created NavBar component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,10 +1,12 @@
 import { FC } from "react";
 import { Header } from "../../components/header";
+import { NavBar } from "../../components/navbar";
 
 const App: FC = () => {
   return (
     <>
       <Header />
+      <NavBar />
     </>
   );
 };
