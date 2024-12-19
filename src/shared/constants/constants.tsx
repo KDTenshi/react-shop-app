ff4ce17608b599bfc36765feaaee828e37b9b791
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-19 18:12:37 +0300

    Created IconButton component for UI Kit and changed Header buttons

---
+++ src/shared/constants/constants.tsx
@@ -0,0 +1,7 @@
+import { IconType } from "../types/types";
+
+export const ICONS: { [key in IconType]: string } = {
+  user: "account_circle",
+  fav: "favorite",
+  cart: "shopping_cart",
+};
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,6 @@
 import Logo from "./logo/Logo";
 import Icon from "./icon/Icon";
 import Button from "./button/Button";
+import IconButton from "./iconButton/IconButton";
 
-export { Logo, Icon, Button };
+export { Logo, Icon, Button, IconButton };
