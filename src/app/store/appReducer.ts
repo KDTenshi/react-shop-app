47232ec4b12b9e7aeffe8498e79fe3f0192a9312
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-20 17:26:55 +0300

    Connected app to the API and created requests for API endpoints

--- src/app/store/appReducer.ts
+++ src/app/store/appReducer.ts
@@ -1,3 +1,6 @@
 import { combineReducers } from "@reduxjs/toolkit";
+import { api } from "../../shared/api/api";
 
-export const appReducer = combineReducers({});
+export const appReducer = combineReducers({
+  [api.reducerPath]: api.reducer,
+});
--- src/app/store/appStore.ts
+++ src/app/store/appStore.ts
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch, useSelector } from "react-redux";
 import { appReducer } from "./appReducer";
+import { api } from "../../shared/api/api";
 
 export const appStore = configureStore({
   reducer: appReducer,
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof appStore.getState>;
