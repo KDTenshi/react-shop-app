73c5d23606836edf17fb27aecd4f5cb429e3053d
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-20 17:36:49 +0300

    Added DisplayCases on page and fixed db

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,12 +1,21 @@
 import { FC } from "react";
 import { Header } from "../../components/header";
 import { NavBar } from "../../components/navbar";
+import { useGetBestProductsQuery, useGetPopularProductsQuery, useGetSaleProductsQuery } from "../../shared/api/api";
+import { DisplayCase } from "../../components/displayCase";
 
 const App: FC = () => {
+  const { data: best } = useGetBestProductsQuery();
+  const { data: sale } = useGetSaleProductsQuery();
+  const { data: popular } = useGetPopularProductsQuery();
+
   return (
     <>
       <Header />
       <NavBar />
+      {best && <DisplayCase title="Best of all time" items={best} />}
+      {sale && <DisplayCase title="On sale now" items={sale} />}
+      {popular && <DisplayCase title="Popular" items={popular} />}
     </>
   );
 };
