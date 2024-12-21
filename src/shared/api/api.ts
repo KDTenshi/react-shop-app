672afbac8b809c5738b3234d84bd133bd5e536e2
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-21 13:55:31 +0300

    Edited DisplayCase and API requests

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,27 +1,14 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { Item } from "../types/types";
+import { DisplayCaseType, Item } from "../types/types";
 
 export const api = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
   endpoints: (builder) => ({
-    getSaleProducts: builder.query<Item[], void>({
-      query: () => "sale",
-    }),
-
-    getBestProducts: builder.query<Item[], void>({
-      query: () => "best",
-    }),
-
-    getPopularProducts: builder.query<Item[], void>({
-      query: () => "popular",
-    }),
-
-    getAllProducts: builder.query<Item[], void>({
-      query: () => "all",
+    getProducts: builder.query<Item[], DisplayCaseType | void>({
+      query: (type) => (type ? type : ""),
     }),
   }),
 });
 
-export const { useGetAllProductsQuery, useGetBestProductsQuery, useGetPopularProductsQuery, useGetSaleProductsQuery } =
-  api;
+export const { useGetProductsQuery } = api;
--- src/shared/types/types.ts
+++ src/shared/types/types.ts
@@ -4,6 +4,8 @@ export type ButtonPropsDefault = ButtonHTMLAttributes<HTMLButtonElement>;
 
 export type IconType = "fav" | "user" | "cart";
 
+export type DisplayCaseType = "best" | "popular" | "sale";
+
 export type Item = {
   id: string;
   name: string;
