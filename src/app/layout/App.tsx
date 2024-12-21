9ceb35ec78383583b383940fe6eaee0561e7a704
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-21 15:05:06 +0300

    Added Banner component

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -2,12 +2,14 @@ import { FC } from "react";
 import { Header } from "../../components/header";
 import { NavBar } from "../../components/navbar";
 import { DisplayCase } from "../../components/displayCase";
+import { Banner } from "../../components/banner";
 
 const App: FC = () => {
   return (
     <>
       <Header />
       <NavBar />
+      <Banner />
       <DisplayCase type="best" />
       <DisplayCase type="popular" />
       <DisplayCase type="sale" />
