787fd55641ce5b1c499fbc549a1da83f1ef87647
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-21 14:29:30 +0300

    Created ItemCardSkeleton component

--- src/components/displayCase/ui/DisplayCase.tsx
+++ src/components/displayCase/ui/DisplayCase.tsx
@@ -3,6 +3,7 @@ import { DisplayCaseType } from "../../../shared/types/types";
 import style from "./DisplayCase.module.css";
 import { ItemCard } from "../../itemCard";
 import { useGetProductsQuery } from "../../../shared/api/api";
+import ItemCardSkeleton from "../../itemCard/ui/ItemCardSkeleton";
 
 interface DisplayCaseProps {
   type?: DisplayCaseType;
@@ -15,7 +16,7 @@ const titles: { [key in DisplayCaseType]: string } = {
 };
 
 const DisplayCase: FC<DisplayCaseProps> = ({ type }) => {
-  const { data } = useGetProductsQuery(type);
+  const { data, isLoading } = useGetProductsQuery(type);
   const selectedItems = data && data.slice(0, 4);
 
   const title = type ? titles[type] : "Display case";
@@ -24,6 +25,7 @@ const DisplayCase: FC<DisplayCaseProps> = ({ type }) => {
     <div className={style.DisplayCase}>
       <h2 className={style.Title}>{title}</h2>
       <div className={style.Wrapper}>
+        {isLoading && [...new Array(4)].map((_, index) => <ItemCardSkeleton key={index} />)}
         {selectedItems && selectedItems.map((item) => <ItemCard key={item.id} item={item} saleMark={item.onSale} />)}
       </div>
     </div>
