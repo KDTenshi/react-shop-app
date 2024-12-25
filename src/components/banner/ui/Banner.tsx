bb30bddc4ac0ea7e50a2569fc2f1e168052fa1bd
Артур Мыльников <kuraidatenshidesu@gmail.com>
2024-12-25 14:02:07 +0300

    Added API endpoints for Banners

--- src/components/banner/ui/Banner.tsx
+++ src/components/banner/ui/Banner.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { Image } from "../../../shared/ui";
 import style from "./Banner.module.css";
+import { useGetBigBannerQuery } from "../../../shared/api/api";
 
 const Banner: FC = () => {
-  const image = "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg";
+  const { data } = useGetBigBannerQuery();
 
-  return <Image src={image} alt={"Banner"} className={style.Banner} />;
+  return <>{data && <Image src={data.image} alt={"Banner"} className={style.Banner} />}</>;
 };
 
 export default Banner;
--- src/components/itemsBanner/ui/ItemsBanner.tsx
+++ src/components/itemsBanner/ui/ItemsBanner.tsx
@@ -1,40 +1,32 @@
 import { FC } from "react";
 import { ItemsGroupType } from "../../../shared/types/types";
 import style from "./ItemsBanner.module.css";
+import { useGetPageBannersQuery } from "../../../shared/api/api";
 
 interface ItemsBannerProps {
   type: ItemsGroupType;
 }
 
-const content: { [key in ItemsGroupType]: { title: string; image: string } } = {
-  best: {
-    title: "The best figurines and models you could find",
-    image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-  },
-  popular: {
-    title: "The most popular figurines and models",
-    image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-  },
-  sale: {
-    title: "Figurines and models that are currently on sale",
-    image: "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
-  },
-};
-
 const ItemsBanner: FC<ItemsBannerProps> = ({ type }) => {
+  const { data } = useGetPageBannersQuery();
+
   return (
-    <div className={style.ItemsBanner} style={{ backgroundImage: `url(${content[type].image})` }}>
-      <div className={style.Text}>
-        <h2 className={style.Title}>{content[type].title}</h2>
-        <p className={style.Description}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dignissimos ullam illo possimus eveniet
-          iure et alias sit, repellendus modi porro voluptatum harum libero cum magnam incidunt repellat exercitationem!
-          Eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dignissimos ullam illo possimus
-          eveniet iure et alias sit, repellendus modi porro voluptatum harum libero cum magnam incidunt repellat
-          exercitationem! Eaque?
-        </p>
-      </div>
-    </div>
+    <>
+      {data && (
+        <div className={style.ItemsBanner} style={{ backgroundImage: `url(${data[type].image})` }}>
+          <div className={style.Text}>
+            <h2 className={style.Title}>{data[type].title}</h2>
+            <p className={style.Description}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dignissimos ullam illo possimus
+              eveniet iure et alias sit, repellendus modi porro voluptatum harum libero cum magnam incidunt repellat
+              exercitationem! Eaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur dignissimos
+              ullam illo possimus eveniet iure et alias sit, repellendus modi porro voluptatum harum libero cum magnam
+              incidunt repellat exercitationem! Eaque?
+            </p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
